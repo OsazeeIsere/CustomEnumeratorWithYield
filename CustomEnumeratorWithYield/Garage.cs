e7015230f063f0ec3f55d2220b7d81865843d4d3
Osazee Isere <oz1.concepts@gmail.com>
2021-06-26 02:29:30 +0100

    initial commit

---
+++ CustomEnumeratorWithYield/Garage.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+namespace CustomEnumeratorWithYield
+{
+    //garage contains a set of cars
+   public class Garage : IEnumerable
+    {
+        private Car[] carArray = new Car[4];
+        
+        //fill with some car objects upon startup
+        public Garage()
+        {
+            carArray[0] = new Car("Bitto", 80);
+            carArray[1] = new Car("Cruz", 40);
+            carArray[2] = new Car("Big for nothing", 70);
+            carArray[3] = new Car("Pencil Light", 60);
+
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            //this will get thrown immediately
+            throw new Exception("this will get called");
+
+            return actualImplementation();
+
+            IEnumerator actualImplementation()
+            { 
+                foreach (Car c in carArray)
+                {
+                    yield return c;
+                }
+            }
+            
+        }
+        public IEnumerable GetTheCars(bool returnedReversed)
+
+        {
+            //do some error checking here
+            return actualImplementation();
+
+            IEnumerable actualImplementation()
+            {
+                //return the items in reversed
+                if (returnedReversed)
+                {
+                    for(int i=carArray.Length; i != 0; i--)
+                    {
+                        yield return (carArray[i - 1]);
+                    }
+                }
+                else
+                {
+                    //rturned the car as arranged in the  array
+                    foreach(Car c in carArray)
+                    {
+                       yield return c;
+                    }
+                }
+            }
+
+        }
+
+        
+    }
+}
--- CustomEnumeratorWithYield/Program.cs
+++ CustomEnumeratorWithYield/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace CustomEnumeratorWithYield
 {
@@ -10,6 +11,24 @@ namespace CustomEnumeratorWithYield
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("fun with yield keyword");
+            Garage carLot = new Garage();
+            //get items using the GetEnumerator
+            foreach(Car c in carLot)
+            {
+                Console.WriteLine("{0} is going {1} MPH", c.PetName, c.CurrSpeed);
+                Console.WriteLine();
+                            
+            }
+            // get items in reverse using named iterator
+            foreach (Car c in carLot.GetTheCars(true))
+            {
+                Console.WriteLine("{0} is going {1} MPH", c.PetName, c.CurrSpeed);
+
+            }
+            Console.ReadLine();
+
         }
+        
     }
 }
